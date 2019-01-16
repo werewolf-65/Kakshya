2d25ff801137180f927576c47dbd5dcfc145cfcb
werewolf-65 <saileshsthnp@gmail.com>
2019-01-16 18:52:39 +0545

    Able to update the blog secretly!

--- blog/views.py
+++ blog/views.py
@@ -1,23 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 # Create your views here.
-posts=[
-    {
-    'author':'Subodh',
-    'title':'Blog Post 1',
-    'content':'First post in this site',
-    'date_posted':'November 28,2018',
-    },
-    {
-    'author':'Mandeep',
-    'title':'Blog Post 2',
-    'content':'First post in this site',
-    'date_posted':'November 29,2018',
-    },
-]
+#Dummy Data
+# posts=[
+#     {
+#     'author':'Subodh',
+#     'title':'Blog Post 1',
+#     'content':'First post in this site',
+#     'date_posted':'November 28,2018',
+#     },
+#     {
+#     'author':'Mandeep',
+#     'title':'Blog Post 2',
+#     'content':'Second post in this site',
+#     'date_posted':'November 29,2018',
+#     },
+# ]
 def home(request):
-    context={'posts':posts}
+    context={'posts':Post.objects.all()}
     return render(request,'blog/home.html',context)
 
 def about(request):
-    return render(request,'blog/about.html',{'title':'About'})
+    return render(request,'blog/about.html',{'title' :'About'})
