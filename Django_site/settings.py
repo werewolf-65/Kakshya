acae936fd041c03cb415af2861f6b552f5bf6e0c
werewolf-65 <saileshsthnp@gmail.com>
2019-01-17 00:46:36 +0545

    Added crispy_forms and the form looks neat :)

--- Django_site/settings.py
+++ Django_site/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',
-    'users.apps.UsersConfig'
+    'users.apps.UsersConfig',
+    'crispy_forms'
 ]
 
 MIDDLEWARE = [
@@ -120,3 +121,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+CRISPY_TEMPLATE_PACK='bootstrap4'
