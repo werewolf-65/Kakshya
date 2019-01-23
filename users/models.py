c669a6b2fe4399577004e65cec5e83e32cb71c4c
werewolf-65 <saileshsthnp@gmail.com>
2019-01-23 10:11:56 +0545

    Auto-resize-the-profile-photo feature added

--- users/models.py
+++ users/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
+
 # Create your models here.
 
 class Profile(models.Model):
@@ -7,3 +9,13 @@ class Profile(models.Model):
     image=models.ImageField(default='default.png',upload_to='profile_pics')
     def __str__(self):
         return f'{self.user.username} Profile'
+
+    def save(self):
+        super().save() #run the save method of parent
+
+        img = Image.open(self.image.path)
+
+        if img.height >300 or img.width > 300:
+            output_size=(300,300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
