3bc4811fbca8b163a088c912c80eb32799f65f3e
werewolf-65 <saileshsthnp@gmail.com>
2019-01-30 15:43:10 +0545

    Link to view posts by a part. user added

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from django.http import HttpResponse
+from django.contrib.auth.models import User
 from .models import Post
 from django.contrib.auth.mixins import LoginRequiredMixin,UserPassesTestMixin
 from django.views.generic import (
@@ -7,7 +8,8 @@ from django.views.generic import (
             DetailView,
             CreateView,
             UpdateView,
-            DeleteView)
+            DeleteView,
+            )
 # Create your views here.
 def home(request):
     context={'posts':Post.objects.all()}
@@ -22,12 +24,18 @@ class PostListView(ListView):
     context_object_name='posts'
     ordering=['-date_posted']
     paginate_by=4
-# #For Pagination
-# class UserPostListView(ListView):
-#     model=Post
-#     template_name='blog/user_posts.html' #<app>/<model>_<viewtype>.html
-#     context_object_name='posts'
-#     ordering=['-date_posted']
+
+class UserPostListView(ListView):
+    model=Post
+    template_name='blog/user_posts.html' #<app>/<model>_<viewtype>.html
+    context_object_name='posts'
+    # ordering=['-date_posted']
+    paginate_by=4
+
+    def get_queryset(self):
+        user=get_object_or_404(User,username=self.kwargs.get('username'))
+        return Post.objects.filter(author=user).order_by('-date_posted')
+
 
 class PostDetailView(DetailView):
     model=Post
